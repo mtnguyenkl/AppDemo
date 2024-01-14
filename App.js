4a61e00641c29151fdde60904c576e5e704c1cd1
ngocnm <ngocmn1205@gmail.com>
2024-01-14 16:13:36 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {Text} from 'react-native';
+import Lab3 from ".src/sreens/lab3";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  return <Lab3></Lab3>
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/screens/lab3/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Lab3 = () => {
+  return <Text>Lab3</Text>;
+};
+
+export default Lab3;
