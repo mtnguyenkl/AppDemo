54a62e0bf9bb400148137daab213eeca98942615
ngocnm <ngocmn1205@gmail.com>
2024-01-14 16:30:44 +0700

    Bo sung lab5

---
+++ src/screens/lab4/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Lab4 = () => {
+  return <Text>Lab4</Text>;
+};
+
+export default Lab4;
---
+++ src/screens/lab5/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const MyLab5 = () => {
+  return <Text>Lab5</Text>;
+};
+
+export default MyLab5;
